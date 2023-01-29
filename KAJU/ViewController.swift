6bde98765b3cc08db94d44d25de3bfb3be076e77
Duhan Boblanlı <77344408+duhanboblanli@users.noreply.github.com>
2023-01-29 17:33:45 +0300

    Initial Commit

---
+++ KAJU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KAJU
+//
+//  Created by Duhan Boblanlı on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
