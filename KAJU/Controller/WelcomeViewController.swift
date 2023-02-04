0fa8bff76eb8c8eaca4e7fa3d3374879d98efc55
Duhan Boblanlı <77344408+duhanboblanli@users.noreply.github.com>
2023-02-05 01:13:28 +0300

    WelcomeViewController added -UI updated -Segues added

---
+++ KAJU/Controller/WelcomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WelcomeViewController.swift
+//  KAJU
+//
+//  Created by Duhan Boblanlı on 4.02.2023.
+//
+
+import UIKit
+
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var getStartedButton: UIButton!
+    
+    @IBOutlet weak var logInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupButtonStyle(button: getStartedButton, cornerRadius: 0.11)
+        setupButtonStyle(button: logInButton, cornerRadius: 0.09)
+    }
+    
+    
+    //Kenarlardan 10,height 85 constraintsli buttonlar için ideal cornerRadius 0.11
+    //Kenarlardan 10,height 65 constraintsli buttonlar için ideal cornerRadius 0.09
+    func setupButtonStyle(button : UIButton,cornerRadius: Float){
+        button.layer.cornerRadius = CGFloat(cornerRadius) * button.bounds.size.width
+        button.clipsToBounds = true
+    }
+    
+    
+    
+    
+    
+}
+
